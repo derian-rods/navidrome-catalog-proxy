43b26877226a7ee85fc04eab3678802b43c62700
derianRS <derian.rods@gmail.com>
2026-08-20 15:13:39 +0200

    Validate Subsonic auth through Navidrome

--- src/subsonic/routes.js
+++ src/subsonic/routes.js
@@ -1,19 +1,14 @@
-import { okResponse } from './responses.js';
 import { proxyToNavidrome } from '../navidrome/client.js';
 import { search2, search3 } from './search.js';
 import { stream } from './stream.js';
 import { getCoverArt, getSong } from './media.js';
 
 export async function registerSubsonicRoutes(app) {
-  app.get('/rest/ping.view', async () => okResponse());
-  app.get('/rest/ping', async () => okResponse());
+  app.get('/rest/ping.view', async (request, reply) => proxyToNavidrome(request, reply));
+  app.get('/rest/ping', async (request, reply) => proxyToNavidrome(request, reply));
 
-  app.get('/rest/getLicense.view', async () => okResponse({
-    license: { valid: true }
-  }));
-  app.get('/rest/getLicense', async () => okResponse({
-    license: { valid: true }
-  }));
+  app.get('/rest/getLicense.view', async (request, reply) => proxyToNavidrome(request, reply));
+  app.get('/rest/getLicense', async (request, reply) => proxyToNavidrome(request, reply));
 
   app.get('/rest/search3.view', async request => search3(request));
   app.get('/rest/search3', async request => search3(request));
